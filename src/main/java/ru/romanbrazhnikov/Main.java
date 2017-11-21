285defea2b32f199618ec8984fa438a62b102912
Roman <mail@romanbrazhnikov.ru>
2017-11-21 19:29:48 +0700

    Scraper class added.

--- src/main/java/ru/romanbrazhnikov/Main.java
+++ src/main/java/ru/romanbrazhnikov/Main.java
@@ -1,7 +1,10 @@
 package ru.romanbrazhnikov;
 
+import ru.romanbrazhnikov.agilescraper.AgileScraper;
+
 public class Main {
     public static void main(String[] args){
-
+        AgileScraper scraper = new AgileScraper();
+        scraper.run();
     }
 }
