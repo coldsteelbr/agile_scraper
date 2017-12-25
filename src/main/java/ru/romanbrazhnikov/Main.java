415e6a36038e9c7823e4b19e60de28e4e1497387
Roman <mail@romanbrazhnikov.ru>
2017-12-25 13:42:19 +0700

    Minor corrections, new manifest for JAR

--- src/main/java/ru/romanbrazhnikov/Main.java
+++ src/main/java/ru/romanbrazhnikov/Main.java
@@ -9,7 +9,9 @@ import ru.romanbrazhnikov.agilescraper.hardcodedconfigs.PrimitiveConfiguration;
 
 public class Main {
     private static final String SPRAN_FLATS = "conf_spran_flat_sale.prs";
+    private static final String SPRAN_COMM_SALE = "conf_spran_comm_sale.prs";
     private static final String SPRAN_FLATS_TEST = "conf_spran_flat_sale_test.prs";
+
     public static void main(String[] args){
         AgileScraper scraper = new AgileScraper();
         HardcodedConfigFactory configFactory = new HardcodedConfigFactory();
@@ -20,7 +22,8 @@ public class Main {
         //PrimitiveConfiguration configuration = configFactory.getUpWorkDataScrapingJobs();
 
         PrimitiveConfigBuilder builder = new PrimitiveConfigBuilder();
-        builder.readFromXmlFile(SPRAN_FLATS);
+        String configToDownload = SPRAN_FLATS;
+        builder.readFromXmlFile(configToDownload);
         PrimitiveConfiguration configuration = builder.init();
         //System.out.println(configuration.getDebugInfo());
 
